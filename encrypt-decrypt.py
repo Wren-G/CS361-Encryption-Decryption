6f73d9ec30971ff6c377a487e97863d40a2ac239
Fern Burke <burkege@oregonstate.edu>
2025-11-24 01:36:53 +0000

    Inconsistent Conventions - FB Commit

--- encrypt-decrypt.py
+++ encrypt-decrypt.py
@@ -1,176 +1,166 @@
 import time
 import os
 
-charType = ''
+
+charType = ""
 publicKeys = {
-    # add in a set of (n, e) keys here (ex. these are small n!)
     444446005879: 679
 }
 privateKeys = {
-    # add in a set of (n, d) keys here (ex. for small n)
     444446005879: 1309115239
 }
 
+
 # ENCRYPTION
-def encrypt_prepare_chunks(rawTxt):
-    # establish lists
-    
+def encryptPrepareChunks(rawText):
     chunkList = []
-    encrypted_chunks = []
+    encryptedChunks = []
 
-    # divide text into smaller chunks (a list of)
-    while rawTxt:
-        new, rawTxt = helper_chunkText(rawTxt)
+    while rawText:
+        new, rawText = helperChunkText(rawText)
         chunkList.append(new)
-    return chunkList, encrypted_chunks
 
-def encrypt_process_chunks(chunkList, encrypted_chunks):
-    # encode chunks, then RSA encrypt them
+    return chunkList, encryptedChunks
+
+
+def encryptProcessChunks(chunkList, encryptedChunks):
     for chunk in chunkList:
-        this_chunk = helper_encode_output(helper_encode_ints(chunk))
+        thisChunk = helperEncodeOutput(helperEncodeInts(chunk))
+        thisEncryptedChunk = pow(
+            thisChunk,
+            publicKeys[444446005879],
+            444446005879
+        )
+        encryptedChunks.append(thisEncryptedChunk)
 
-        # x^e mod n
-        this_encrypted_chunk = pow(this_chunk, publicKeys[444446005879], 444446005879)
+    return encryptedChunks
 
-        # add to list
-        encrypted_chunks.append(this_encrypted_chunk)
-    return encrypted_chunks
 
-def encrypt_write_output(encrypted_chunks, outputPath):
-    to_write = ''
-    for chunk in encrypted_chunks:
-        to_write += (str(chunk))
-        to_write += ' '
-    
-    with open(outputPath, 'w') as f:
-        f.write(to_write)
-    return to_write
+def encryptWriteOutput(encryptedChunks, outputPath):
+    toWrite = ""
+    for chunk in encryptedChunks:
+        toWrite += str(chunk)
+        toWrite += " "
 
-def encrypt(rawTxt, outputPath):
-    chunkList, encrypted_chunks = encrypt_prepare_chunks(rawTxt)
-    encrypted_chunks = encrypt_process_chunks(chunkList, encrypted_chunks)
-    return encrypt_write_output(encrypted_chunks, outputPath)
+    with open(outputPath, "w") as f:
+        f.write(toWrite)
+
+    return toWrite
+
+
+def encrypt(rawText, outputPath):
+    chunkList, encryptedChunks = encryptPrepareChunks(rawText)
+    encryptedChunks = encryptProcessChunks(chunkList, encryptedChunks)
+    return encryptWriteOutput(encryptedChunks, outputPath)
+
 
 # DECRYPTION
-def decrypt_split(rawTxt):
-    # Decrypt the text using a key
-    # divide the string from file into numbers
-    strs = rawTxt.split()
+def decryptSplit(rawText):
+    strs = rawText.split()
     nums = []
-    for str in strs:
-        num = int(str)
-        nums.append(num)
+    for s in strs:
+        nums.append(int(s))
+
     return nums
 
-def decrypt_process_split(nums):
-    text = ''
+
+def decryptProcessSplit(nums):
+    text = ""
     for num in nums:
-        # decrypt based on d mod n
         new = pow(num, privateKeys[444446005879], 444446005879)
-        partial = helper_decode(new) # returns a partial string
+        partial = helperDecode(new)
         text += partial
-    
-        # eliminate whitespace if necessary
-        new_text = text.rstrip()
-    return new_text
+        newText = text.rstrip()
 
-def decrypt_write_output(new_text, outputPath):
-    # return text and append to file.
-    with open(outputPath, 'w') as f:
-        f.write(new_text)
-    return new_text
+    return newText
 
-def decrypt(rawTxt, outputPath):
-    nums = decrypt_split(rawTxt)
-    new_text = decrypt_process_split(nums)
-    return decrypt_write_output(new_text, outputPath)
 
-# ENCRYPT/DECRYPT HELPER FUNCTIONS
-def helper_chunkText(rawTxt):
-    # Helper function to split string into correct block sizes
-    # base case for recursion
-    if len(rawTxt) < 4:
-        textChunk = rawTxt
-        to_add = 4 - len(textChunk)
-        for i in range(1, to_add):
-            textChunk += ' '    
-        rawTxt = ""
+def decryptWriteOutput(newText, outputPath):
+    with open(outputPath, "w") as f:
+        f.write(newText)
 
-    # take first 4 chars
+    return newText
+
+
+def decrypt(rawText, outputPath):
+    nums = decryptSplit(rawText)
+    newText = decryptProcessSplit(nums)
+    return decryptWriteOutput(newText, outputPath)
+
+
+# HELPERS
+def helperChunkText(rawText):
+    if len(rawText) < 4:
+        textChunk = rawText
+        toAdd = 4 - len(textChunk)
+        for _ in range(1, toAdd):
+            textChunk += " "
+        rawText = ""
     else:
-        textChunk = rawTxt[:4]
-        # remove those chars from rawTxt
-        rawTxt = rawTxt[4:]
+        textChunk = rawText[:4]
+        rawText = rawText[4:]
 
-    # return both cases (we call this function recursively)
-    return textChunk, rawTxt
+    return textChunk, rawText
 
-def helper_encode_ints(chars):
-    # Encodes 4 character blocks into a number.
+
+def helperEncodeInts(chars):
     ints = []
-    # use ord() to convert to ascii
     if len(chars) < 4:
         for i in range(0, len(chars) - 1):
-            new = ord(chars[i])
-            ints.append(new)
-        
-        space = ord(' ')
+            ints.append(ord(chars[i]))
+
+        space = ord(" ")
         while len(ints) != 4:
             ints.append(space)
     else:
-    # use ord() to convert to ascii
         for i in range(0, 4):
-            new = ord(chars[i]) # ord is an integer
-            ints.append(new)
+            ints.append(ord(chars[i]))
+
     return ints
 
-def helper_encode_output(ints):
+
+def helperEncodeOutput(ints):
     output = 0
-    # implement division algorithm
     for i in range(0, 4):
-        new = ints[i] * (128 ** i)
-        output += new
-    
-    # return ascii ints
+        output += ints[i] * (128 ** i)
+
     return output
 
-def helper_decode(n):
-    # This function reverses the encoding process into characters
-    # implement division algorithm reversed
+
+def helperDecode(n):
     chars = []
-    for i in range(1, 5):
+    for _ in range(4):
         new = n % 128
         chars.append(new)
-        n = n // 128
-    
-    # use chr() to convert to characters
-    my_string = ''
+        n //= 128
+
+    my_string = ""
     for num in chars:
-        char_for_str = chr(num) # returns a string
-        # concatenate all elements together
-        my_string += char_for_str
-    
+        my_string += chr(num)
+
     return my_string
 
-# MAIN HELPER FUNCTIONS
 
-def main_helper_read(inputPath):
-    while True: # This loops reads the txt file once per second
-        # sleep while waiting for input.txt
-        with open(inputPath, "r") as f: 
-            return f.read() # variable holding the text
-        
-def main_helper_write(inputPath):
+# MAIN HELPERS
+def mainHelperRead(inputPath):
+    while True:
+        with open(inputPath, "r") as f:
+            return f.read()
+
+
+def mainHelperWrite(inputPath):
     with open(inputPath, "w") as f:
-            f.write("") # this erases the input text so that the program does not get confused
+        f.write("")
 
-def main_helper_char(charType, rawTxt, outputPath):
-    if (charType == 'e'): #first char e = encryption mode
-        encrypt(rawTxt, outputPath)
-    elif (charType == 'd'): #first char d = decryption mode
-        decrypt(rawTxt, outputPath)
+
+def mainHelperChar(charType, rawText, outputPath):
+    if charType == "e":
+        encrypt(rawText, outputPath)
+    elif charType == "d":
+        decrypt(rawText, outputPath)
     else:
         print("Error: Main program sent unrecognized first character.")
+
 
 # MAIN
 def main():
@@ -179,20 +169,20 @@ def main():
     outputPath = os.path.join(baseDir, "output.txt")
 
     while True:
-        rawTxt = main_helper_read(inputPath)
-        if not rawTxt: # if nothing read, sleep then repeat
+        rawText = mainHelperRead(inputPath)
+
+        if not rawText:
             time.sleep(1)
             continue
 
-        # once input.txt has text in it,
-        # take the first char off the file and store in charType
-        charType = rawTxt[0]
-        # remove the first character from the text file
-        rawTxt = rawTxt[1:] # this just leaves the rest of the string minus that first char
-        
-        main_helper_write(inputPath)
-        main_helper_char(charType, rawTxt, outputPath)
+        charType = rawText[0]
+        rawText = rawText[1:]
+
+        mainHelperWrite(inputPath)
+        mainHelperChar(charType, rawText, outputPath)
+
         time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
